06919ad210b9770fa951f96baf710c9b42e6c2d3
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-04-08 14:59:38 +0200

    add dump

---
+++ DATA/taskdb_dump.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE taskdb;
+USE taskdb;
+
+CREATE TABLE tasks (
+[id] INT PRIMARY KEY IDENTITY,
+[timestamp] DATETIME NOT NULL DEFAULT GETDATE(),
+[text] TEXT NULL,
+[checked] BIT NOT NULL DEFAULT 0);
+
+INSERT INTO tasks ([text], [checked]) VALUES
+('first test', 0),
+('second test', 1),
+('third test', 1);
